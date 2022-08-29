ba9019191d3d9009aba988fec43a87e3f48802d1
bschuwer <112434040+bschuwer@users.noreply.github.com>
2022-08-29 22:58:37 +0200

    Add files via upload

---
+++ music.js
@@ -0,0 +1,16 @@
+
+img = document.getElementById("weight-el")
+
+
+      function enlargeImg() {
+        img.style.transform = "scale(1.2)";
+        img.style.transition = "transform 0.3s ease";
+      
+        
+      
+      }
+
+ 
+      
+        
+    
